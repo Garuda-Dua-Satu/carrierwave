d76d3cc072c14191736135c269f7350996579ffc
hezbymuhammad <hezbymuhammad@gmail.com>
2021-05-24 11:11:05 +0700

    patch activerecord.rb to fix stack too deep

--- lib/carrierwave/orm/activerecord.rb
+++ lib/carrierwave/orm/activerecord.rb
@@ -77,10 +77,9 @@ module CarrierWave
         end
 
         def remove_#{column}!
+          super
           self.remove_#{column} = true
           write_#{column}_identifier
-          self.remove_#{column} = false
-          super
         end
 
         # Reset cached mounter on record reload
